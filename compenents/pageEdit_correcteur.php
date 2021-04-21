bedc3f07a6ddd139c96b40047e7c8686af7b258a
Dejeanty <julie.dejeanty@gmail.com>
2021-04-21 15:50:40 +0200

    EditCorrection

---
+++ compenents/pageEdit_correcteur.php
@@ -0,0 +1,23 @@
+<div class="container">
+    <div class="editPart">
+        <div class="undo-redo">
+            <a href=""><i class="fas fa-undo-alt"></i></a>
+            <a href=""><i class="fas fa-redo-alt"></i></a>
+        </div>
+
+        <a class='supprimer' href=""><i class="fas fa-trash"></i> Effacer le texte</a>
+    </div>
+
+    <div class="suggestions">
+        <div class="first">
+            <h3>Suggestions</h3>
+            
+            <div class="probleme">
+                <div class="cercle"></div>
+            </div>
+        </div>
+    <div>
+
+</div>
+
+
