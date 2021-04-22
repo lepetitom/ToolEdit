5cb28fdec454e9520233b145f079aa72bb8b63f3
Jenna <jennamicheau.esd@gmail.com>
2021-04-22 11:23:31 +0200

    test 2

--- compenents/dico.php
+++ compenents/dico.php
@@ -1,5 +1,21 @@
-<div> test
-</div>
-<h1>
-test</h1>
-<h2>tessssssssssssssssssst</h2>
+<div class="dictionnaire">
+    <h1>Dictionnaire</h1>
+    <div class="navSearch">
+        <input type="text" placeholder="Mot à définir">
+        <input type="submit">
+    </div>
+    <div class="result">
+        <div class="word"></div>
+        <div class="defition">
+            <ul>
+            <li>Commencement ; origine ; source : cause première.</li>
+            <li>Commencement ; origine ; source : cause première.</li>
+            <li>Commencement ; origine ; source : cause première.</li>
+            <li>Commencement ; origine ; source : cause première.</li>
+            <li>Commencement ; origine ; source : cause première.</li>
+            <li>Commencement ; origine ; source : cause première.</li>
+            <li>Commencement ; origine ; source : cause première.</li>
+            </ul>
+        </div>
+    </div>
+</div> 
